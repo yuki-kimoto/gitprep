82ad893042ca14e9838442cc0426dd948a2c5422
Yuki Kimoto <kimoto.yuki@gmail.com>
2012-12-12 23:09:12 +0900

    added tree page

--- lib/Gitprep/API.pm
+++ lib/Gitprep/API.pm
@@ -58,5 +58,43 @@ sub parse_id_path {
   return ($id, $path);
 }
 
+sub trees {
+  my ($self, $rep, $tid, $ref, $dir) = @_;
+  
+  my $c = $self->cntl;
+  
+  my $git = $c->app->git;
+  
+  # Get tree (command "git ls-tree")
+  my @entries = ();
+  my $show_sizes = 0;
+  open my $fh, '-|', $git->cmd($rep), 'ls-tree', '-z',
+      ($show_sizes ? '-l' : ()), $tid
+    or $self->croak('Open git-ls-tree failed');
+  {
+    local $/ = "\0";
+    @entries = map { chomp; $git->dec($_) } <$fh>;
+  }
+  close $fh
+    or $self->croak(404, "Reading tree failed");
+
+  # Parse tree
+  my $trees;
+  for my $line (@entries) {
+    my $tree = $git->parse_ls_tree_line($line, -z => 1, -l => $show_sizes);
+    $tree->{mode_str} = $git->_mode_str($tree->{mode});
+    
+    # Commit log
+    my $name = defined $dir ? "$dir/$tree->{name}" : $tree->{name};
+    my $commit_log = $git->latest_commit_log($rep, $ref, $name);
+    $tree = {%$tree, %$commit_log};
+    
+    push @$trees, $tree;
+  }
+  $trees = [sort {$b->{type} cmp $a->{type} || $a->{name} cmp $b->{name}} @$trees];
+  
+  return $trees;
+}
+
 1;
 
