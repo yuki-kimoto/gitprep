79e7a0253c48c75e7de834fc3a29c410db0d6974
Yuki Kimoto <kimoto.yuki@gmail.com>
2013-02-01 12:34:38 +0900

    added commit page diff tree header

--- lib/Gitprep/Main.pm
+++ lib/Gitprep/Main.pm
@@ -4,72 +4,6 @@ use File::Basename 'dirname';
 use Carp 'croak';
 use Gitprep::API;
 
-sub blobdiff {
-  my $self = shift;
-
-  # Parameters
-  my $project_ns = $self->param('project');
-  my $project = "/$project_ns";
-  my $home_ns = dirname $project_ns;
-  my $home = "/$home_ns";
-  my $diff = $self->param('diff');
-  my $file = $self->param('file');
-  my $from_file = $self->param('from-file');
-  $from_file = $file unless defined $from_file;
-  my $plain = $self->param('plain');
-  my $from_id;
-  my $id;
-  if ($diff =~ /\.\./) { ($from_id, $id) = $diff =~ /(.+)\.\.(.+)/ }
-  else { $id = $diff }
-  
-  # Git
-  my $git = $self->app->git;
-
-  # Get blob diff (command "git diff")
-  open my $fh, '-|', $git->cmd($project), 'diff', '-r', '-M', '-p',
-      $from_id, $id, '--', $from_file, $file
-    or croak "Open git-diff-tree failed";
-  
-  # Blob diff plain
-  if ($plain) {
-    # Content
-    my $content = do { local $/; <$fh> };
-    close $fh;
-    
-    # Render
-    my $content_disposition .= "inline; filename=$file";
-    $self->res->headers->content_disposition($content_disposition);
-    $self->res->headers->content_type("text/plain; charset=" . $git->encoding);
-    $self->render(data => $content);
-  }
-  
-  # Blob diff
-  else {
-    # Lines
-    my @lines = map { $git->dec($_) } <$fh>;
-    close $fh;
-    my $lines = $self->_parse_blobdiff_lines(\@lines);
-    
-    # Commit
-    my $commit = $git->parse_commit($project, $id);
-    
-    # Render
-    $self->render(
-      '/blobdiff',
-      home => $home,
-      home_ns => $home_ns,
-      project => $project,
-      project_ns => $project_ns,
-      id => $id,
-      from_id => $from_id,
-      file => $file,
-      from_file => $from_file,
-      commit => $commit,
-      lines => $lines
-    );
-  }
-}
-
 sub snapshot {
   my $self = shift;
 
@@ -116,35 +50,6 @@ sub snapshot {
     $c->write_chunk($buffer, $cb);
   };
   $self->$cb;
-}
-
-sub tag {
-  my $self = shift;
-  
-  # Parameters
-  my $project_ns = $self->param('project');
-  my $project = "/$project_ns";
-  my $home_ns = dirname $project_ns;
-  my $home = "/$home_ns";
-  my $id = $self->param('id');
-  
-  # Git
-  my $git = $self->app->git;
-  
-  # Ref names
-  my $tag  = $git->parse_tag($project, $id);
-  my $author_date
-    = $git->parse_date($tag->{author_epoch}, $tag->{author_tz});
-  $tag->{author_date} = $git->timestamp($author_date);
-  
-  # Render
-  $self->render(
-    home => $home,
-    home_ns => $home_ns,
-    project => $project,
-    project_ns => $project_ns,
-    tag => $tag,
-  );
 }
 
 sub _quote_command {
