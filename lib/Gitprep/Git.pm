303191b8d78a1ed90cc860a7b0915176a618a289
Yuki Kimoto <kimoto.yuki@gmail.com>
2013-02-02 15:00:24 +0900

    cleanup

--- lib/Gitprep/Git.pm
+++ lib/Gitprep/Git.pm
@@ -322,6 +322,42 @@ sub id_by_path {
   return $id;
 }
 
+sub root_ns {
+  my ($self, $root) = @_;
+
+  $root =~ s/^\///;
+  
+  return $root;
+}
+
+sub parse_object {
+  my ($self, $project, $id_path) = @_;
+  
+  # Parse id and path
+  my $refs = $self->references($project);
+  my $id;
+  my $path;
+  for my $rs (values %$refs) {
+    for my $ref (@$rs) {
+      $ref =~ s#^heads/##;
+      $ref =~ s#^tags/##;
+      if ($id_path =~ s#^\Q$ref(/|$)##) {
+        $id = $ref;
+        $path = $id_path;
+        last;
+      }      
+    }
+  }
+  unless (defined $id) {
+    if ($id_path =~ s#(^[^/]+)(/|$)##) {
+      $id = $1;
+      $path = $id_path;
+    }
+  }
+  
+  return ($id, $path);
+}
+
 sub path_by_id {
   my $self = shift;
   my $project = shift;
