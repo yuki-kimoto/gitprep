2c200e3148fe4388240507df2b09ddbcc1aec51d
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-05-27 22:21:48 +0900

    Fix CGI PATH_INFO encoding bug

--- lib/Gitprep.pm
+++ lib/Gitprep.pm
@@ -1,5 +1,10 @@
 use 5.010001;
 package Gitprep;
+
+use Encode ();
+# Fix CGI PATH_INFO encoding bug
+eval { $ENV{PATH_INFO} = Encode::decode('UTF-8', $ENV{PATH_INFO}) };
+
 use Mojo::Base 'Mojolicious';
 
 use Carp 'croak';
