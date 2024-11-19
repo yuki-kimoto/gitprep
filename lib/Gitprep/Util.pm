683cf31f79b6472815632a9329e1fa89f8056910
Patrick Monnerat <patrick@monnerat.net>
2024-11-19 14:38:49 +0100

    glob2regex: convert a glob pattern into a regular expression
    $regex = Gitprep::Util::glob2regex($glob_pattern)

--- lib/Gitprep/Util.pm
+++ lib/Gitprep/Util.pm
@@ -38,4 +38,109 @@ sub fingerprint {
 }
 
 
+sub glob2regex {
+  my ($glob) = @_;
+
+  # Translate a glob pattern into a regular expression.
+
+  my $regex;
+
+  # Translate wildcards '*', '**' and '?'.
+  local *wildcard = sub {
+    my ($glob) = @_;
+    my $regex;
+    my $min = 0;
+    my $nomax;
+
+    if ($glob =~ /^\*{2,}(.*)$/) {
+      $nomax = 1;
+      $regex = '.';
+      $glob = $1;
+    } else {
+      $regex = '[^/]';
+      # Optimize consecutive wildcards gathering min and max counts.
+      while ($glob =~ /^(\?|\*(?!\*))(.*)$/) {
+        $min++ if $1 eq '?';
+        $nomax = 1 if $1 eq '*';
+        $glob = $2;
+      }
+    }
+
+    # Generate repetition counts.
+    $regex .= '*' if !$min;
+    $regex .= '+' if $min == 1 && $nomax;
+    $regex .= '{' . $min . ($nomax? ',': '') . '}' if $min > 1;
+    return ($glob, $regex);
+  };
+
+  # Translate [] sets.
+  local *set = sub {
+    my ($glob) = @_;
+    my $regex = '';
+    if ($glob =~ /^[\^!](.*)$/) {
+      $regex = '^';
+      $glob = $1;
+    }
+    # Convert set content.
+    while ($glob =~ /^([^\]]|$)(.*)$/) {
+      if ($1 ne '-') {
+        $2 =~ /^(.)(.*)$/ if $1 eq '\\' && $2;
+        ;
+      }
+      $regex .= $1;
+      $glob = $2;
+    }
+    return (substr($glob, 1), "[$regex]");
+  };
+
+  # Translate {,} groups.
+  local *group = sub {
+    my ($glob) = @_;
+    my $regex;
+
+    while ($glob =~ /^[^}]/) {
+      if ($glob =~ /^,+(.*)$/) {
+        $glob = $1;
+        $regex .= '|';
+      } else {
+        ($glob, my $term) = terms($glob, 1);
+        $regex .= $term;
+      }
+    }
+    return (substr($glob, 1), "(?:$regex)");
+  };
+
+  # Translate a sequence of terms.
+  local *terms = sub {
+    my ($glob, $ingroup) = @_;
+    my $regex;
+    my $re;
+
+    while ($glob =~ /^(.)(.*)$/) {
+      last if $ingroup && $1 =~ /[,}]/;
+      if ($1 eq '[') {
+        ($glob, $re) = set($2);
+      } elsif ($1 eq '{') {
+        ($glob, $re) = group($2);
+      } elsif ($1 =~ /[?*]/) {
+        ($glob, $re) = wildcard($glob);
+      } else {
+        $2 =~ /^(.)(.*)$/ if $1 eq '\\';
+        $glob = $2;
+        $re = $1;
+        $re =~ s/[\$^+*?.=!|\\()[{}]/\\$&/;
+      }
+      $regex .= $re;
+    }
+    return ($glob, $regex);
+  };
+
+  while ($glob =~ /./) {
+    ($glob, my $re) = terms($glob);
+    $regex .= $re;
+  }
+  return "^$regex\$";
+}
+
+
 1;
